1c284697d6e44db66b4a876ec5e508d27ced0f71
qjwvtd <176156562@qq.com>
2018-04-07 18:28:53 +0800

    2018-4-7

--- server/404.js
+++ server/404.js
@@ -5,17 +5,11 @@
 var fs = require('fs');
 module.exports = function(app){
     // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
+    app.get('*',function(req, res) {
         var err = new Error('Not Found');
         err.status = 404;
         console.log('已发送404页面');
         console.log(err);
-        //解析图片发送到前端
-        var filePath = './resource/404.jpg';
-        var bData = fs.readFileSync(filePath);
-        var base64Str = bData.toString('base64');
-        var dataUri = 'data:image/jpeg;base64,' + base64Str;
-        res.writeHeader(404,{'content-type' : 'text/html;charset="utf-8"'});
-        res.end('<div style="text-align:center"><img src="'+dataUri+'" style="width:50%"></div>');
+        res.sendFile( __dirname + "/" + "resource/404.html" );
     });
 };
